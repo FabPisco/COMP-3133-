8823ad3ff0da32bdc460250872fed00f1c4be8ca
FabPisco <fmonkeyrock@icloud.com>
2025-01-26 23:31:45 -0500

    hello

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+export function add(i, j) {
+    return i + j;
+}
+
+
+export function sub(i, j) {
+    return i - j;
+}
+
+
+export function mul(i, j) {
+    return i * j;
+}
+
+export function div(i, j) {
+    if (j === 0) throw new Error('Division by zero');
+    return i / j;
+}
